becaba2f168aa7977d3dc72d175109324d145640
Ryan McCuaig <ryan@ryanmccuaig.net>
2014-09-05 07:40:22 -0700

    Sketch in first semi-working, untested version

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: "User-level feature flippers",
+  version: "1.0.0",
+});
+
+Package.on_use(function(api) {
+  api.use(['underscore', 'accounts-base']);
+  api.add_files('lib/flippers.js');
+  if (api.export) {
+    api.export('Flipper');
+  }
+});
+
+Package.on_test(function(api) {
+  api.use('tinytest');
+  api.use('flippers');
+  api.add_files('test/flippers.js');
+});
---
+++ test/flippers.js
@@ -0,0 +1,5 @@
+// TODO tests, dork.
+
+Tinytest.add('example', function (test) {
+  test.equal(true, false);
+});
